d7504d576c139cf666a684b1e8a23c7183ca05c9
Juancho0312 <78771437+Juancho0312@users.noreply.github.com>
2021-02-09 13:58:06 -0600

    Write code to detect eyes from faces
    Using eye detector in python

--- pupiltracker.py
+++ pupiltracker.py
@@ -15,6 +15,15 @@ def detect_and_display(frame):
     faces = face_cascade.detectMultiScale(frame_gray)
     faces = sorted(faces, key=lambda f: f[2] * f[3], reverse=True)
 
+    
+    for (x, y, w, h) in faces[:1]:
+        center = (x + w//2, y + h//2)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        faceROI = frame_gray[y:y+h,x:x+w]
+
+        # In each face, detect eyes
+        eyes = eyes_cascade.detectMultiScale(faceROI, 1.3, 10)
+
 
 cap = cv.VideoCapture(0)
 
