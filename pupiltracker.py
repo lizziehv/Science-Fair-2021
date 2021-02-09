008480f3ff250565835d1c860e2d74c626be2c93
dubysmoreno14 <78758811+dubysmoreno14@users.noreply.github.com>
2021-02-09 13:47:08 -0600

    Start trained models from python library for face recognition

--- pupiltracker.py
+++ pupiltracker.py
@@ -1,3 +1,7 @@
 from __future__ import print_function
 import cv2 as cv
 import numpy as np
+
+
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eyes_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_eye.xml')
