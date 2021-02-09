4b4daaf0b5c4bfe99e33d24cc221e463d9ad2261
andreachain04 <78758734+andreachain04@users.noreply.github.com>
2021-02-09 13:48:55 -0600

    Wrote program to start video capture

--- pupiltracker.py
+++ pupiltracker.py
@@ -5,3 +5,20 @@ import numpy as np
 
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eyes_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_eye.xml')
+
+
+cap = cv.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    detect_and_display(frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
